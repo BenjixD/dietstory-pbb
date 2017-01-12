cc6f755b867b655809b4ad6ac59bee737e9feebf
Tim <tim.blokk@gmail.com>
2017-01-12 17:02:14 +0100

    See last commit

--- src/handling/handlers/npc/QuickMoveRequestHandler.java
+++ src/handling/handlers/npc/QuickMoveRequestHandler.java
@@ -3,14 +3,26 @@ package handling.handlers.npc;
 import client.MapleClient;
 import handling.PacketHandler;
 import handling.RecvPacketOpcode;
-import handling.channel.handler.NPCHandler;
+import script.npc.NPCScriptManager;
+import server.life.MapleLifeFactory;
+import server.life.MapleNPC;
 import tools.data.LittleEndianAccessor;
 
 public class QuickMoveRequestHandler {
 
     @PacketHandler(opcode = RecvPacketOpcode.QUICK_MOVE_REQUEST)
     public static void handle(MapleClient c, LittleEndianAccessor lea){
-        NPCHandler.NPCTalk(lea, c, c.getPlayer());
+        int npcid = lea.readInt();
+        MapleNPC npc = MapleLifeFactory.getNPC(npcid);
+
+        if (NPCScriptManager.getInstance().hasScript(c, npc.getId(), null)) { //I want it to come before shop
+            NPCScriptManager.getInstance().start(c, npc.getId(), null);
+        } else if (npc.hasShop()) {
+            c.getPlayer().setConversation(1);
+            npc.sendShop(c);
+        } else {
+            NPCScriptManager.getInstance().start(c, npc.getId(), null);
+        }
     }
 
 }
